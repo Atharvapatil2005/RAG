af4b3c6be88d311f747f422b5b72020e41271621
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-01 14:23:55 +0530

    defined class vectore store

---
+++ vector_store.py
@@ -0,0 +1,14 @@
+import faiss
+import numpy as np
+import faiss
+import numpy as np
+
+class VectorStore:
+    def __init__(self, embeddings):
+        self.dimension = embeddings.shape[1]
+        self.index = faiss.IndexFlatL2(self.dimension)
+        self.index.add(embeddings)
+
+    def search(self, query_vector, k=2):
+        distances, indices = self.index.search(query_vector, k)
+        return indices[0]
