63fa7f66052f11bf747f09916461b30f30f55752
Atharva Patil <atharvaspatil2005@gmail.com>
2026-07-29 18:52:21 +0530

    fix:hf model

--- secure_rag/generator.py
+++ secure_rag/generator.py
@@ -7,7 +7,7 @@ from openai import OpenAI#openAI sdk -> hugging face router
 
 _client = None
 _DEFAULT_BASE_URL = "https://router.huggingface.co/v1"#HF router end point 
-_DEFAULT_MODEL = "Qwen/Qwen3-8B:fastest"#llm model
+_DEFAULT_MODEL = "Qwen/Qwen2.5-7B-Instruct"#llm model
 LLM_PROVIDER = os.getenv("LLM_PROVIDER", "ollama").lower()
 
 
